9a10f7d565f537a6a734cfcff1cb0dab7911c1fb
Devansh Jethmalani <jethmalani.devansh@gmail.com>
2020-05-01 00:05:35 +0530

    fix(cloud-functions): try init firebase out of callback

--- cloud-functions/src/index.ts
+++ cloud-functions/src/index.ts
@@ -1,9 +1,10 @@
 import * as functions from "firebase-functions"
 import * as admin from "firebase-admin"
 
+let app = admin.initializeApp()
+
 export const insertResponse = functions.https.onCall(
-    async ({ inquiryId, response }: { inquiryId: string, response: string[] }) => {
-        let app = admin.initializeApp()
+    async ({ inquiryId, response }: { inquiryId: string, response: string[] }) => {        
         await app.firestore().collection(`/inquiries/${inquiryId}/responses`).add({ data: response })
         // TODO: reCaptcha
     }
